70e2c18c0f72e54d4dbbcc22a3f378350187e179
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-07-19 19:50:57 +0900

    0719_FloodFill

---
+++ Client_API/api_ga_factorial/api_ga_factorial.cpp
@@ -0,0 +1,86 @@
+﻿// api_ga_factorial.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+//팩토리얼을 재귀호출을 이용하여 작성
+
+//int factorial(int n)
+//{
+//   
+//    if (n == 1)      // n이 1일 때
+//        return 1;    // 1을 반환하고 재귀호출을 끝냄
+//
+//    return n * factorial(n - 1);    // n과 factorial 함수에 n - 1을 넣어서 반환된 값을 곱함
+//}
+//
+//int main()
+//{
+//    printf("%d\n", factorial(5));
+//
+//    return 0;
+//}
+
+
+
+unsigned int DoFactorial(unsigned int tN);
+
+unsigned int DoFactorialNoRecursion(unsigned int tN);
+
+int main()
+{
+    unsigned int tResult = 0;
+    tResult = DoFactorial(4);
+
+    cout << tResult << endl;
+
+
+    tResult = 0;
+    tResult = DoFactorialNoRecursion(4);
+
+    cout << tResult << endl;
+
+
+    return 0;
+}
+
+
+unsigned int DoFactorial(unsigned int tN)
+{
+    if (0 == tN)
+    {
+        return 1;
+    }
+
+    else
+    {
+        return tN* DoFactorial(tN - 1);
+    }
+}
+
+
+unsigned int DoFactorialNoRecursion(unsigned int tN)
+{
+    unsigned int tResult;
+
+    if (0 == tN)
+    {
+        tResult = 1;
+    }
+
+    else
+    {
+        int tTemp = 1;
+
+        int ti = 0;
+        for (ti = tN; ti > 0; --ti)
+        {
+            tTemp = tTemp * ti;
+        }
+
+        tResult = tTemp;
+    }
+
+    return tResult;
+}
---
+++ Client_API/api_ga_floodfill/api_ga_floodfill.cpp
@@ -0,0 +1,153 @@
+﻿// api_ga_floodfill.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+//스택 자료구조를 사용하겠다(셀의 위치값 기억용도)
+#include <stack>
+using namespace std;
+
+/*
+* 플러드필 알고리즘
+* 
+* 색칠하기 알고리즘.
+* 속도가 너무 느려 색칠하기알고리즘으로 쓰이지는 않고 
+* 응용하여 보드게임의 여러 알고리즘으로 쓰인다
+*/
+
+#define MAX_ROW 6
+#define MAX_COL 6
+
+
+//행과 열(위치값)을 기억시켜두는 자료구조
+stack<int> mIntStack;
+
+
+// 6x6 board
+//0: 빈곳, 1: 칠한곳, 2:벽(경계)
+int mGrid[MAX_ROW][MAX_COL] =
+{
+    2,2,2,2,2,2,
+    2,0,0,0,0,2,
+    2,0,0,0,0,2,
+    2,2,2,2,2,2,
+    2,0,0,0,0,2,
+    2,2,2,2,2,2,
+};
+
+
+
+void DoFloodFill(int tCol, int tRow);
+
+void DoFloodFillNoRecursion(int tCol, int tRow);
+
+void DisPlayGrid();
+
+int main()
+{
+    cout << "before" << endl;
+    DisPlayGrid();
+
+    cout << endl;
+    //DoFloodFill(2, 1);
+    DoFloodFillNoRecursion(1, 4);
+
+    cout << "after" << endl;
+    DisPlayGrid();
+
+    return 0;
+}
+
+
+void DoFloodFill(int tCol, int tRow)
+{
+    if (1 == mGrid[tRow][tCol] || 2 == mGrid[tRow][tCol])
+    {
+        //색을 칠하지않고 그냥둔다 (암것도 안한다)
+        return;
+    }
+
+    else
+    {
+        //칠한다
+        mGrid[tRow][tCol] = 1;
+
+        DoFloodFill(tCol - 1, tRow); //좌
+        DoFloodFill(tCol + 1, tRow); //우
+        DoFloodFill(tCol, tRow - 1); //상
+        DoFloodFill(tCol, tRow + 1); //하
+    }
+}
+
+
+void DoFloodFillNoRecursion(int tCol, int tRow)
+{
+    while (!mIntStack.empty())
+    {
+        mIntStack.pop();
+    }
+
+
+    //floodfill
+    //맨 처음 시드셀의 위치를 자료구조에 담는다
+    mIntStack.push(tCol);
+    mIntStack.push(tRow);
+
+
+    while ( !mIntStack.empty())
+    {
+        tRow = mIntStack.top();
+        mIntStack.pop();
+
+        tCol = mIntStack.top();
+        mIntStack.pop();
+
+        if (1 == mGrid[tRow][tCol] || 2 == mGrid[tRow][tCol])
+        {
+            continue;
+        }
+
+        //칠한다
+        mGrid[tRow][tCol] = 1;
+
+        //좌
+        mIntStack.push(tCol - 1);
+        mIntStack.push(tRow);
+
+        //우
+        mIntStack.push(tCol + 1);
+        mIntStack.push(tRow);
+
+
+        //상
+        mIntStack.push(tCol);
+        mIntStack.push(tRow - 1);
+
+        //하
+        mIntStack.push(tCol);
+        mIntStack.push(tRow + 1);
+    }
+}
+
+void DisPlayGrid()
+{
+    int tRow = 0;
+    int tCol = 0;
+
+    string tString = "";
+    char tszTemp[256] = {};
+
+    for (tRow = 0; tRow < MAX_ROW; tRow++)
+    {
+        for (tCol = 0; tCol < MAX_COL; tCol++)
+        {
+            sprintf_s(tszTemp, "%d", mGrid[tRow][tCol]);
+
+            tString = tString + tszTemp;
+        }
+
+        tString = tString + "\n";
+    }
+
+    cout << tString;
+}
+
