e44e4a8fa1ddf8b71dd0d57c65c2709a50ff6a3a
kmw7691 <101631103+JeongEunChang@users.noreply.github.com>
2022-07-25 19:05:26 +0900

    0725

---
+++ Client_API/winAPIMatchPuzzle/winAPIMatchPuzzle_step_5/winAPIMatchPuzzle/CPiece.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "CUnit.h"
+
+
+class CPiece: public CUnit
+{
+public:
+	CPiece()
+	{
+
+	}
+	virtual ~CPiece()	//가상소멸자 
+	{
+
+	}
+	CPiece(const CPiece& t):CUnit(t)
+	{
+
+	}
+
+	void operator=(const CPiece& t)
+	{
+		CUnit::operator=(t);
+	}
+
+public:
+	virtual CObjectRyu* clone() override
+	{
+		return new CPiece(*this);
+	};
+};
+
